2149b320d42250e91f653d2cc1811263a5158de3
dahighsky <aakashuttkarsh29@gmail.com>
2022-08-16 17:16:41 +0530

    quiz flow complete

--- assets/script.js
+++ assets/script.js
@@ -39,40 +39,119 @@ const questions = [
     },
   ];
 
+let users = [
+  {
+    userName: "Aakash",
+    userScore: 69
+  }
+]
+
+var score = 0;
+
 function quiz(){
   console.log("here")
   let question = document.getElementById("quest");
   let opt = document.querySelectorAll(".quest-opt")
   let result = document.getElementById("check")
 
-  var score = 0;
   newQuestion();
   function newQuestion(questionIndex = 0){
-    question.innerHTML = questions[questionIndex].questionText
-    console.log("iteration check 1")
-    opt.forEach(function(text, index){
-      opt[index].innerText = questions[questionIndex].options[index]
-      console.log("iteration check 2")
-      opt[index].addEventListener("click", function(){
-        if(index == questions[questionIndex].answer){
-          result.innerText = "Correct!"
-          score++
-          console.log("herehere")
-          questionIndex++
-          newQuestion(questionIndex)
-        }
-        else{
-          result.innerText = "Incorrect!"
-          console.log("hehehe")
-          questionIndex++
-          newQuestion(questionIndex)
-        }
+    if(questionIndex < questions.length){
+      question.innerHTML = questions[questionIndex].questionText
+      console.log("iteration check 1")
+      opt.forEach(function(text, index){
+        opt[index].innerText = questions[questionIndex].options[index]
+        console.log("iteration check 2", questionIndex)
+        opt[index].addEventListener("click", function(){
+          if(index == questions[questionIndex].answer){
+            result.innerText = "Correct!"
+            score++
+            console.log("herehere")
+            questionIndex++
+            newQuestion(questionIndex)
+          }
+          else{
+            result.innerText = "Incorrect!"
+            console.log("hehehe")
+            questionIndex++
+            newQuestion(questionIndex)
+          }
+        })
       })
-    })
-    console.log(score)
+      console.log(score)
+    }
+    else{
+      displayScore(score)
+    }
   }  
 }
 
+function displayScore(score){
+  document.getElementById("quiz").style.display = "none";
+  document.getElementById("result").style.display = "flex";
+  console.log(score)
+  document.getElementById("show-score").innerText = score;
+  document.getElementById("initials").addEventListener("keyup", function(event){
+    uName = event.target.value
+    console.log(uName)
+  })
+
+  document.getElementById("save-user").addEventListener("click", function(){
+    console.log(uName + "user here")
+    if(uName){
+      addUser(uName, score)
+      console.log("User here")
+    }
+  })
+}
+
+function addUser(uName, score){
+  users.push({userName: uName,
+              userScore: score})
+  showHighscores()
+}
+
+document.getElementById("leaderboard").addEventListener("click", showHighscores)
+
+function showHighscores(){
+  document.getElementById("intro").style.display = "none";
+  document.getElementById("quiz").style.display = "none";
+  document.getElementById("check").style.display = "none";
+  document.getElementById("result").style.display = "none";
+  document.getElementById("highscores").style.display = "flex";
+  
+  var scoreList = ""
+
+  users.forEach(function(value, index){
+    rank = index + 1
+    scoreEntry = `${rank}. ${users[index].userName} - ${users[index].userScore} <br>`
+    scoreList = scoreList + scoreEntry
+  })
+
+  scoresView = document.getElementById("scores")
+  scoresView.innerHTML = scoreList
+
+  document.getElementById("go-back").addEventListener("click", restart)
+  document.getElementById("clear-scores").addEventListener("click", clearHighscores)
+}
+
+function clearHighscores(){
+  users = [
+    {
+      userName: "Aakash",
+      userScore: 69
+    }
+  ]
+  showHighscores()
+}
+
+function restart(){
+  document.getElementById("intro").style.display = "flex";
+  document.getElementById("quiz").style.display = "none";
+  document.getElementById("check").style.display = "none";
+  document.getElementById("result").style.display = "none";
+  document.getElementById("highscores").style.display = "none";
+}
 function startQuiz(){
   document.getElementById("intro").style.display = "none";
   document.getElementById("quiz").style.display = "flex";
